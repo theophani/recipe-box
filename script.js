0be593037b6062eefd764d915021b7403d4139d2
Tiffany Conroy <tiffany.conroy@gmail.com>
2013-10-19 23:19:35 +0200

    recipe list

--- script.js
+++ script.js
@@ -62,11 +62,6 @@ function build_box (recipe_ingredients) {
   return box;
 }
 
-function prepare_ingredients (recipe_ingredients) {
-  var box = build_box(recipe_ingredients);
-  ingredients_list(box);
-}
-
 function ingredients_list (box) {
   var selected = [];
   var also_with_selected = [];
@@ -89,7 +84,7 @@ function ingredients_list (box) {
   }
 
   function ingredient_class (ingredient) {
-    var also_with = 0;
+    var also_with;
 
     if (!selected.length) {
       return "ingredient";
@@ -128,16 +123,81 @@ function ingredients_list (box) {
     return item;
   }
 
+  function broadcast () {
+    var data = { detail: { selected_ingredients: selected } };
+    var evt = new CustomEvent('ingredient:selected', data)
+    document.dispatchEvent(evt);
+  }
+
   items = Object.keys(box.ingredients).sort().map(make_item);
   items.forEach(function (li) { ul.appendChild(li); });
-  document.body.appendChild(ul);
+  document.querySelector('.ingredients_list').appendChild(ul);
 
   ul.addEventListener('click', function (e) {
     if (e.target.tagName !== 'LI') return;
     var ingredient = box.ingredients[e.target.dataset.ingredientKey];
     toggle(ingredient);
     render();
+    broadcast();
   });
+}
+
+function recipes_list (box) {
+  var ul = document.createElement('ul');
+  var items;
+
+  function recipe_class (recipe) {
+    var has_selected = 0;
+
+    if (!selected_ingredients.length) {
+      return "recipe";
+    }
+
+    selected_ingredients.forEach(function (ingredient) {
+      if (recipe.ingredients.indexOf(ingredient) > -1) {
+        has_selected++;
+      }
+    });
+
+    if (has_selected === selected_ingredients.length) {
+      return "recipe selected";
+    } else {
+      return "recipe";
+    }
+  }
+
+  function render () {
+    var any_selected = !!selected_ingredients.length;
+    ul.className = any_selected ? "filtered" : "";
+    items.forEach(function (item) {
+      var recipeKey = item.dataset.recipeKey;
+      var recipe = box.recipes[recipeKey];
+      item.className = recipe_class(recipe);
+    });
+  }
+
+  function make_item (recipe) {
+    var item = document.createElement('li');
+    item.innerHTML = recipe;
+    item.className = "recipe";
+    item.dataset.recipeKey = recipe;
+    return item;
+  }
+
+  items = Object.keys(box.recipes).sort().map(make_item);
+  items.forEach(function (li) { ul.appendChild(li); });
+  document.querySelector('.recipes_list').appendChild(ul);
+
+  document.addEventListener('ingredient:selected', function (e) {
+    selected_ingredients = e.detail.selected_ingredients;
+    render();
+  });
+}
+
+function prepare_ingredients (recipe_ingredients) {
+  var box = build_box(recipe_ingredients);
+  ingredients_list(box);
+  recipes_list(box);
 }
 
 ajax({
